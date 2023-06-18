f9d5de2d5d752b07dcc6ade9edc743b51bb36445
Mehedi Ahsan <mdmehedicse01@gmail.com>
2023-06-18 21:56:18 +0600

    category name added

--- src/Pages/Home/Categories/Category.js
+++ src/Pages/Home/Categories/Category.js
@@ -5,13 +5,13 @@ const Category = ({category}) => {
     const {_id, img, name} = category;
     return (
         <Link to={`/category/${_id}`} aria-label="View Item">
-        <div className="relative overflow-hidden transition duration-500 transform shadow-lg hover:-translate-y-1 hover:shadow-xl flex justify-center items-center  border border-rose-500 w-64 h-48 rounded-3xl">
+        <div className="relative overflow-hidden transition duration-500 transform shadow-lg  hover:shadow-xl flex justify-center items-center  border-2 border-neutral hover:border-primary w-60 h-56 rounded-full">
           {/* <img
             className="object-cover w-52 p-10 h-52"
             src={img}
             alt=""
           /> */}
-          <h1 className='font-bold text-5xl font-Berkshire text-primary'>{name}</h1>
+          <h1 className='font-bold text-5xl font-Berkshire text-accent'>{name}</h1>
         </div>
       </Link>
     );
--- src/Pages/Products/Products.js
+++ src/Pages/Products/Products.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import Product from './Product';
 
 const Products = () => {
     const products = useLoaderData([]);
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        axios.get('https://laptop-resale-server.vercel.app/categories')
+        .then(data => {
+            setCategories(data.data);
+        })
+    },[])
+
+    const categoryName = categories.map(category => {
+        if(category._id === products[0]?.category_id){
+            return category.name;
+        }
+    })
+
 
     return (
         <div className='my-20'>
-            <h1 className='text-3xl md:text-3xl font-semibold text-center text-primary mb-20'>All <span className='text-red-500'>Products</span></h1>
+            <h1 className='text-3xl md:text-3xl font-semibold text-center text-accent mb-20'>{categoryName} {products.length}<span className=''> Products Available 💻</span></h1>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7'>
                 {
                     products.map(product => <Product key={product._id} product={product}></Product>)
