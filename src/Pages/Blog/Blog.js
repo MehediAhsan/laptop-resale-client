3e68bd94e67804679b5d4dc62cf6781810a181fb
Mehedi Ahsan <mdmehedicse01@gmail.com>
2023-06-20 22:29:37 +0600

    designed updated

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -5,9 +5,9 @@ const Blog = () => {
     
 <section class="my-20">
     <div class="container px-6 py-10 mx-auto">
-        <h1 class="text-3xl font-semibold capitalize lg:text-4xl">Our latest blog</h1>
+        <h1 data-aos="fade-right" data-aos-duration="1000" class="text-3xl font-semibold capitalize lg:text-4xl">Our latest blog</h1>
 
-        <div class="grid grid-cols-1 gap-8 mt-8 md:mt-16 md:grid-cols-2">
+        <div data-aos="fade-up" data-aos-duration="1000" class="grid grid-cols-1 gap-8 mt-8 md:mt-16 md:grid-cols-2">
             <div class="lg:flex">
                 <img class="object-cover w-full h-56 rounded-lg lg:w-64" src="https://images.unsplash.com/photo-1515378960530-7c0da6231fb1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt=""/>
 
