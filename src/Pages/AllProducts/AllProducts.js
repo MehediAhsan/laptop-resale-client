43013e4f0c129759730dd2bd8a3f288e9191571b
Mehedi Ahsan <mdmehedicse01@gmail.com>
2023-07-31 22:52:03 +0600

    added voice controller in search bar

--- src/Pages/AllProducts/AllProducts.js
+++ src/Pages/AllProducts/AllProducts.js
@@ -37,6 +37,25 @@ const AllProducts = () => {
     setCurrentPage(1);
   };
 
+  // Handle voice search
+  const handleVoiceSearch = () => {
+    if ('webkitSpeechRecognition' in window) {
+      const recognition = new window.webkitSpeechRecognition();
+      recognition.continuous = false;
+      recognition.interimResults = false;
+      recognition.lang = 'en-US';
+
+      recognition.onresult = (event) => {
+        const transcript = event.results[0][0].transcript;
+        setSearchTerm(transcript);
+      };
+
+      recognition.start();
+    } else {
+      console.log('Web Speech API is not supported in this browser.');
+    }
+  };
+
   // Perform search
   useEffect(() => {
     const filteredProducts = products.filter((product) =>
@@ -78,6 +97,12 @@ const AllProducts = () => {
           onChange={handleSearchChange}
           className='px-4 py-2 border-2 rounded-md focus:outline-none focus:ring-2 focus:ring-secondary text-neutral text-lg font-semibold'
         />
+        <button
+          onClick={handleVoiceSearch}
+          className='ml-2 px-3 py-1 border-2 rounded-md focus:outline-none focus:ring-2 focus:ring-secondary bg-rose-500 text-white font-bold'
+        >
+          🎤
+        </button>
       </div>
       {loading ? (
         <Loader />
