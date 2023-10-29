10d31acb541f602ab80817eb29bc58833dce9807
Mehedi Ahsan <mdmehedicse01@gmail.com>
2023-10-29 11:40:50 +0600

    update blog section

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -29,84 +29,67 @@ const Blog = () => {
         },
         // Add more discussions as needed
       ];
+
+      const blogs = [
+        {
+          "id": 1,
+          "image": "https://source.unsplash.com/WLUHO9A_xik/800x600",
+          "title": "Exploring Nature's Wonders",
+          "date": "2023-10-20"
+        },
+        {
+          "id": 2,
+          "image": "https://images.unsplash.com/photo-1698434156086-918aa526b531?auto=format&fit=crop&q=80&w=1470&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+          "title": "The Art of Photography",
+          "date": "2023-10-20"
+        },
+        {
+          "id": 3,
+          "image": "https://images.unsplash.com/photo-1602080858428-57174f9431cf?auto=format&fit=crop&q=80&w=1551&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+          "title": "Culinary Adventures: Tasting the World",
+          "date": "2023-10-20"
+        },
+        {
+          "id": 4,
+          "image": "https://images.unsplash.com/photo-1593642634524-b40b5baae6bb?auto=format&fit=crop&q=80&w=1632&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+          "title": "Urban Exploration: Capturing City Vibes",
+          "date": "2023-10-20"
+        },
+        {
+          "id": 5,
+          "image": "https://images.unsplash.com/photo-1487017159836-4e23ece2e4cf?auto=format&fit=crop&q=80&w=1471&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+          "title": "Unraveling the Universe: Space Exploration",
+          "date": "2023-10-20"
+        },
+        {
+          "id": 6,
+          "image": "https://source.unsplash.com/XtUd5SiX464/800x600",
+          "title": "The Joy of Reading: Literary Escapes",
+          "date": "2023-10-20"
+        }
+      ]
+      
   return (
     
-<section class="my-20">
+<section class="my-14">
     <div class="container px-6 py-10 mx-auto">
-        <h1 data-aos="fade-right" data-aos-duration="1000" class="text-3xl font-semibold capitalize lg:text-4xl text-center pb-10">Our latest blog</h1>
+        <h1 data-aos="fade-right" data-aos-duration="1000" class="text-2xl font-semibold capitalize md:text-3xl text-center pb-8">Our latest blog</h1>
 
         <div data-aos="fade-up" data-aos-duration="1000" class="grid grid-cols-1 gap-14 mt-8 md:mt-16 md:grid-cols-2">
-            <div class="lg:flex border rounded-2xl p-2">
-                <img class="object-cover w-full h-56 rounded-lg lg:w-64" src="https://images.unsplash.com/photo-1515378960530-7c0da6231fb1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt=""/>
+            {
+              blogs.map(blog => <div key={blog.id} class="lg:flex shadow shadow-secondary rounded-2xl p-2">
+              <img class="object-cover w-full h-56 rounded-lg lg:w-64 skew-y-1 skew-x-1" src={blog.image} alt=""/>
 
-                <div class="flex flex-col justify-between py-6 lg:mx-6">
-                    <a href="#" class="text-xl font-semibold hover:underline ">
-                        How to use sticky note for problem solving
-                    </a>
-                    
-                    <span class="text-sm">On: 20 October 2019</span>
-                </div>
-            </div>
-
-            <div class="lg:flex border rounded-2xl p-2">
-                <img class="object-cover w-full h-56 rounded-lg lg:w-64" src="https://images.unsplash.com/photo-1497032628192-86f99bcd76bc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt=""/>
-
-                <div class="flex flex-col justify-between py-6 lg:mx-6">
-                    <a href="#" class="text-xl font-semibold hover:underline ">
-                        How to use sticky note for problem solving
-                    </a>
-
-                    <span class="text-sm">On: 20 October 2019</span>
-                </div>
-            </div>
-
-            <div class="lg:flex border rounded-2xl p-2">
-                <img class="object-cover w-full h-56 rounded-lg lg:w-64" src="https://images.unsplash.com/photo-1544654803-b69140b285a1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt=""/>
-
-                <div class="flex flex-col justify-between py-6 lg:mx-6">
-                    <a href="#" class="text-xl font-semibold hover:underline ">
-                        Morning routine to boost your mood
-                    </a>
-
-                    <span class="text-sm">On: 25 November 2020</span>
-                </div>
-            </div>
-
-            <div class="lg:flex border rounded-2xl p-2">
-                <img class="object-cover w-full h-56 rounded-lg lg:w-64" src="https://images.unsplash.com/photo-1530099486328-e021101a494a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1547&q=80" alt=""/>
-
-                <div class="flex flex-col justify-between py-6 lg:mx-6">
-                    <a href="#" class="text-xl font-semibold hover:underline ">
-                        All the features you want to know
-                    </a>
-
-                    <span class="text-sm">On: 30 September 2020</span>
-                </div>
-            </div>
-
-            <div class="lg:flex border rounded-2xl p-2">
-                <img class="object-cover w-full h-56 rounded-lg lg:w-64" src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1484&q=80" alt=""/>
-
-                <div class="flex flex-col justify-between py-6 lg:mx-6">
-                    <a href="#" class="text-xl font-semibold hover:underline ">
-                        Minimal workspace for your inspirations
-                    </a>
-
-                    <span class="text-sm">On: 13 October 2019</span>
-                </div>
-            </div>
-
-            <div class="lg:flex border rounded-2xl p-2">
-                <img class="object-cover w-full h-56 rounded-lg lg:w-64" src="https://images.unsplash.com/photo-1624996379697-f01d168b1a52?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt=""/>
-
-                <div class="flex flex-col justify-between py-6 lg:mx-6">
-                    <a href="/" class="text-xl font-semibold hover:underline ">
-                        What do you want to know about Blockchane
-                    </a>
-                    
-                    <span class="text-sm">On: 20 October 2019</span>
-                </div>
-            </div>
+              <div class="flex flex-col gap-3 justify-evenly py-6 lg:mx-6">
+                  <a href="/" class="text-xl font-semibold hover:underline ">
+                      {blog.title}
+                  </a>
+                  
+                  <span class="">{blog.date}</span>
+                  <button className="shadow shadow-secondary rounded w-20 p-1">See More</button>
+              </div>
+          </div>)
+            }
         </div>
     </div>
 
@@ -119,7 +102,7 @@ const Blog = () => {
           {discussionsData.map((discussion) => (
             <div
               key={discussion.id}
-              className=" rounded-lg shadow-md overflow-hidden"
+              className="rounded-lg shadow overflow-hidden shadow-secondary"
             >
               <img
                 src={discussion.image}
