9cbb1468fa14be7e1d0d9515a6a4a0d792b530e0
Mehedi Ahsan <mdmehedicse01@gmail.com>
2023-12-14 23:45:56 +0600

    card updated

--- src/Pages/Products/Product.jsx
+++ src/Pages/Products/Product.jsx
@@ -1,98 +1,124 @@
-import React, { useEffect, useState } from 'react';
-import toast from 'react-hot-toast';
-import ProductBookModal from './ProductBookModal';
+import React, { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+import ProductBookModal from "./ProductBookModal";
 import { HiLocationMarker } from "react-icons/hi";
+import { IoCheckmarkCircleOutline } from "react-icons/io5";
 
-const Product = ({product}) => {
-    const {picture, product_name, resale_price, seller_email, seller_name, original_price, location, use_years, description, condition, paid, time} = product;
-    const [bookingProduct, setBookingProduct] = useState(null);
-    const [seller, setSeller] = useState({});
+const Product = ({ product }) => {
+  const {
+    picture,
+    product_name,
+    resale_price,
+    seller_email,
+    seller_name,
+    original_price,
+    location,
+    use_years,
+    description,
+    condition,
+    paid,
+    time,
+  } = product;
+  const [bookingProduct, setBookingProduct] = useState(null);
+  const [seller, setSeller] = useState({});
 
-    useEffect(() => {
-        fetch(`https://laptop-resale-server.vercel.app/users/${seller_email}`)
-            .then(res => res.json())
-            .then(data => {
-                setSeller(data);
-            })
-    }, [seller_email])
+  useEffect(() => {
+    fetch(`https://laptop-resale-server.vercel.app/users/${seller_email}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setSeller(data);
+      });
+  }, [seller_email]);
 
-    const handleReportProduct = product => {
-        fetch(`https://laptop-resale-server.vercel.app/reports/${product._id}`, {
-            method: 'PATCH', 
-            headers: {
-                'content-type': 'application/json',
-            },
-            body: JSON.stringify({reported: true})
-        })
-        .then(res => res.json())
-        .then(data => {
-            if(data.modifiedCount > 0) {
-              toast.success('Report admin successfully')
-            }
-            else{
-                toast.error('Already reported this product');
-            }
-        })
-    }
-
-    return (
-        <>
-        { !paid && 
-        <div className="bg-base-100 shadow-md mx-4 md:mx-0 rounded-lg shadow-secondary">
-        <figure><img className='w-full md:h-80 p-4 transition ease-in-out duration-500 hover:scale-105 rounded cursor-pointer' src={picture} alt="Album"/></figure>
-        <div className="card-body pt-0">
-            <div className='flex justify-between'>
-            <h2 className="card-title">{product_name}</h2>
-            <div className='flex flex-row items-center gap-1 font-medium text-lg'>
-                <>{seller_name}</>
-                {
-                    seller.verified && <img className='w-5 h-5' src="https://cdn-icons-png.flaticon.com/512/1828/1828640.png" alt="" />
-                }
-            </div>
-            </div>
-            <p>{description.slice(0,40)}...</p>
-            {/* <span className='font-medium '>Posted Time: {time.slice(0,10)}</span> */}
-            {/* <div className='flex justify-between'>
-            
-            <span className='font-medium capitalize'>Product Condition: {condition}</span>
-            
-            </div> */}
-            
-            
-            
-            <div className='flex justify-between'>
-            <span className='font-medium'>Years of Use: {use_years}years</span>
-            <span className='font-medium'><span className='flex items-center justify-center gap-1'>
-            <HiLocationMarker></HiLocationMarker> <span>{location}</span>
-                </span></span>
-            </div>
-
-           <div className='flex gap-4'>
-            <span className='font-medium text-2xl text-error'>${resale_price}</span>
-            <span className='font-medium line-through text-gray-400 mt-2'>${original_price}</span>
-           </div>
-            
-            
-            <div className='flex justify-between mt-2'>
-                <button onClick={() => handleReportProduct(product)} className=' text-white rounded-full flex justify-center items-center uppercase text-base bg-red-500 py-1 px-3'>Report</button>
-                <div className="card-actions">
-                <label
-                    htmlFor="product-booking-modal"
-                    className="rounded-full uppercase border border-primary hover:bg-primary hover:text-black px-3 py-1 cursor-pointer"
-                    onClick={() => setBookingProduct(product)}
-                >Book Now</label>
-                { 
-                    bookingProduct &&
-                    <ProductBookModal bookingProduct={bookingProduct} setBookingProduct={setBookingProduct} seller_email={seller_email}></ProductBookModal>
-                }   
-                </div>
-            </div>
-            
-        </div>
-        </div>
+  const handleReportProduct = (product) => {
+    fetch(`https://laptop-resale-server.vercel.app/reports/${product._id}`, {
+      method: "PATCH",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ reported: true }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.modifiedCount > 0) {
+          toast.success("Report admin successfully");
+        } else {
+          toast.error("Already reported this product");
         }
-        </>
-    );
+      });
+  };
+
+  return (
+    <>
+      {!paid && (
+        <div className="bg-base-100 shadow mx-4 md:mx-0 rounded shadow-primary">
+          <figure>
+            <img
+              className="w-full h-72 pb-4 rounded"
+              src={picture}
+              alt="Album"
+            />
+          </figure>
+          <div className="card-body pt-0">
+            <div className="flex justify-between">
+              <h2 className="card-title font-medium text-secondary">{product_name}</h2>
+              <div className="flex flex-row items-center justify-center gap-1 font-medium">
+                <span>{seller_name}</span>
+                {seller.verified && (
+                  <IoCheckmarkCircleOutline className="text-xl font-semibold text-neutral" />
+                )}
+              </div>
+            </div>
+            <p className="text-sm font-light">{description.slice(0, 40)}...</p>
+            <div className="flex justify-between">
+              <span className="font-medium">
+                Used: {use_years} years
+              </span>
+              <span className="font-medium">
+                <span className="flex items-center justify-center gap-1">
+                  <HiLocationMarker></HiLocationMarker> <span>{location}</span>
+                </span>
+              </span>
+            </div>
+
+            <div className="flex gap-4">
+              <span className="font-medium text-2xl text-error">
+                ${resale_price}
+              </span>
+              <span className="font-medium line-through text-gray-400 mt-2">
+                ${original_price}
+              </span>
+            </div>
+
+            <div className="flex justify-between mt-2">
+              <button
+                onClick={() => handleReportProduct(product)}
+                className=" text-white rounded-full flex justify-center items-center text-base bg-neutral py-1 px-4"
+              >
+                Report
+              </button>
+              <div className="card-actions">
+                <label
+                  htmlFor="product-booking-modal"
+                  className="rounded-full border border-primary hover:bg-primary hover:text-black px-3 py-1 cursor-pointer"
+                  onClick={() => setBookingProduct(product)}
+                >
+                  Book Now
+                </label>
+                {bookingProduct && (
+                  <ProductBookModal
+                    bookingProduct={bookingProduct}
+                    setBookingProduct={setBookingProduct}
+                    seller_email={seller_email}
+                  ></ProductBookModal>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Product;
