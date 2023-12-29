d6038b4824c5733b8decf9667f9f4f43427e7f2d
Mehedi Ahsan <mdmehedicse01@gmail.com>
2023-12-29 21:50:40 +0600

    delivery man added

--- src/Dashboard/DeliveryMan/DeliveryMan.jsx
+++ src/Dashboard/DeliveryMan/DeliveryMan.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { FaLocationDot, FaPhone } from "react-icons/fa6";
+import { Rating } from "@smastrom/react-rating";
+import "@smastrom/react-rating/style.css";
 
 const DeliveryMan = () => {
   const members = [
@@ -6,77 +9,76 @@ const DeliveryMan = () => {
       avatar: "https://api.uifaces.co/our-content/donated/xZ4wg2Xj.jpg",
       name: "John lorin",
       email: "john@example.com",
+      phone: "01990022990",
+      location: "Dhaka",
+      rating: "5",
     },
     {
       avatar: "https://randomuser.me/api/portraits/men/86.jpg",
       name: "Chris bondi",
       email: "chridbondi@example.com",
+      phone: "01990022990",
+      location: "Khulna",
+      rating: "4",
     },
     {
       avatar:
         "https://images.unsplash.com/photo-1464863979621-258859e62245?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ",
       name: "yasmine",
       email: "yasmine@example.com",
+      phone: "01990022990",
+      location: "Dhaka",
+      rating: "4.5",
     },
     {
       avatar:
         "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=a72ca28288878f8404a795f39642a46f",
       name: "Joseph",
       email: "joseph@example.com",
+      phone: "01990022990",
+      location: "Faridpur",
+      rating: "4",
     },
   ];
   return (
-    <div className="max-w-2xl mx-auto px-4">
-      <div className="items-start justify-between sm:flex">
+    <div className="container w-11/12 mx-auto px-4">
+      <div className="items-start justify-between sm:flex mt-10">
         <div>
-          <h4 className="text-gray-800 text-xl font-semibold">Team members</h4>
-          <p className="mt-2 text-gray-600 text-base sm:text-sm">
-            Give your team members access to manage the system.
+          <h4 className="text-secondary text-xl font-semibold">Delivery Man</h4>
+          <p className="mt-2 text-base sm:text-sm">
+            Choose you delivery man according to your location.
           </p>
         </div>
-        <a
-          href="javascript:void(0)"
-          className="inline-flex items-center justify-center gap-1 py-2 px-3 mt-2 font-medium text-sm text-center text-white bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-700 rounded-lg sm:mt-0"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M12 6v12m6-6H6"
-            />
-          </svg>
-          New member
-        </a>
       </div>
       <ul className="mt-12 divide-y">
         {members.map((item, idx) => (
-          <li key={idx} className="py-5 flex items-start justify-between">
-            <div className="flex gap-3">
+          <li key={idx} className="py-8 flex items-center justify-between">
+            <div className="flex items-center gap-8">
               <img
                 src={item.avatar}
-                className="flex-none w-12 h-12 rounded-full"
+                className="flex-none w-24 h-24 rounded-full"
               />
-              <div>
-                <span className="block text-sm text-gray-700 font-semibold">
+              <div className="flex flex-col gap-1">
+                <span className="block text-xl text-error font-medium">
                   {item.name}
                 </span>
-                <span className="block text-sm text-gray-600">
-                  {item.email}
+                <span className="block">{item.email}</span>
+                <span className="flex items-center gap-1">
+                  <FaPhone />
+                  {item.phone}
+                </span>
+                <span className="flex items-center gap-1 text-secondary">
+                  <FaLocationDot />
+                  {item.location}
                 </span>
               </div>
             </div>
+            <Rating style={{ maxWidth: 100 }} value={item.rating} readOnly />
             <a
               href="javascript:void(0)"
-              className="text-gray-700 text-sm border rounded-lg px-3 py-2 duration-150 bg-white hover:bg-gray-100"
+              className="text-accent text-sm border rounded-lg px-3 py-1 duration-150"
             >
-              Manage
+              Contact
             </a>
           </li>
         ))}
