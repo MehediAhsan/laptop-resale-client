622095ce8fb09d10a2fa318183f23e6f6a04eb7b
Mehedi Ahsan <mdmehedicse01@gmail.com>
2024-01-01 22:27:56 +0600

    install map

--- src/Dashboard/DeliveryLocation/DeliveryLocaion.jsx
+++ src/Dashboard/DeliveryLocation/DeliveryLocaion.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
+import "leaflet/dist/leaflet.css";
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 
 const DeliveryLocaion = () => {
-    return (
-        <div>
-            sffds
-        </div>
-    );
+  return (
+    <MapContainer center={[23.777176, 90.399452]} zoom={13} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[23.777176, 90.399452]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
 };
 
 export default DeliveryLocaion;
